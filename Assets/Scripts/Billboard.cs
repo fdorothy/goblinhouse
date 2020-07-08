bb29a0ecf50eb765bb5d2b8a37242a5f3ebde74e
Fredric Dorothy <fredricdorothy@gmail.com>
2020-07-08 13:14:20 -0500

    Initial commit, basic scene demo with movable sprite.

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 p2 = Camera.main.transform.forward;
+        Vector3 dir = new Vector3(-p2.x, 0.0f, -p2.z);
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpriteLoop
+
+public class Player : MonoBehaviour
+{
+    protected NavMeshAgent agent;
+    public SpriteRenderer sr;
+    public Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse button down");
+            Vector3 target;
+            if (GetMouseTarget(out target))
+            {
+                Debug.Log("got target");
+                agent.SetDestination(target);
+            }
+        }
+    }
+
+    public bool GetMouseTarget(out Vector3 target)
+    {
+        int walkableMask = 1 << NavMesh.GetAreaFromName("Walkable");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.Log(ray.ToString());
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log("got hit");
+            target = hit.point;
+            return true;
+        } else {
+            target = Vector3.zero;
+            return false;
+        }
+    }
+}
