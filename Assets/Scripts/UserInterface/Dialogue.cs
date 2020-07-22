86333aeb46bbecd09231809dd36f0e1b386e7747
Fredric Dorothy <fredricdorothy@gmail.com>
2020-07-21 22:54:54 -0500

    Added some missing files from last commit

---
+++ Assets/Scripts/UserInterface/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text text;
+    public CanvasGroup panel;
+
+    private void Start()
+    {
+        panel.alpha = 0.0f;
+    }
+
+    public void RunDialogue(string text)
+    {
+        this.text.text = text;
+        Sequence seq = DOTween.Sequence();
+        seq.Append(panel.DOFade(1.0f, 0.25f));
+        seq.AppendInterval(1.0f);
+        seq.Append(panel.DOFade(0.0f, 0.25f));
+        seq.OnComplete(() => Destroy(this.gameObject));
+    }
+}
---
+++ Assets/Scripts/UserInterface/DialogueManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Dialogue dialoguePrefab;
+    public Transform dialogueParent;
+    public static DialogueManager singleton;
+
+    public void Awake()
+    {
+        singleton = this;
+    }
+
+    public void CreateDialogue(string text)
+    {
+        Dialogue d = Instantiate<Dialogue>(dialoguePrefab, dialogueParent);
+        d.transform.SetParent(dialogueParent);
+        d.RunDialogue(text);
+    }
+}
