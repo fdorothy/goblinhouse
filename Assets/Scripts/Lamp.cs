2e0886532466b724cc245ec62f8f468cdb869b10
Fredric Dorothy <fredricdorothy@gmail.com>
2024-09-08 12:56:07 -0500

    added power and light flickering

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public MeshRenderer lampShade;
+    public Light lampLight;
+
+    Material onMaterial;
+    public Material offMaterial;
+    float onIntensity;
+    float dstIntensity;
+
+    public void Start()
+    {
+        if (lampShade)
+            onMaterial = lampShade.materials[1];
+        onIntensity = lampLight.intensity;
+        dstIntensity = onIntensity;
+    }
+
+    public void Update()
+    {
+        lampLight.intensity = Mathf.MoveTowards(lampLight.intensity, dstIntensity, Time.deltaTime * 10f);
+    }
+
+    public void TurnOff()
+    {
+        //SetLampMaterial(offMaterial);
+        dstIntensity = 0f;
+    }
+
+    public void TurnOn()
+    {
+        //SetLampMaterial(onMaterial);
+        dstIntensity = onIntensity;
+    }
+
+    public void SetLampMaterial(Material mat)
+    {
+        if (lampShade)
+        {
+            Material[] materials = lampShade.materials;
+            materials[1] = mat;
+            lampShade.materials = materials;
+        }
+    }
+}
---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            TurnOn();
+            yield return new WaitForSeconds(Random.Range(3.0f, 8.0f));
+
+            // flicker
+            for (int i = 0; i < 5; i++)
+            {
+                TurnOff();
+                yield return new WaitForSeconds(Random.Range(.01f, .05f));
+                TurnOn();
+                yield return new WaitForSeconds(Random.Range(.1f, .2f));
+            }
+        }
+    }
+
+    void TurnOff()
+    {
+        foreach (Lamp l in AllLamps())
+            l.TurnOff();
+    }
+    void TurnOn()
+    {
+        foreach (Lamp l in AllLamps())
+            l.TurnOn();
+    }
+
+    Lamp[] AllLamps() => FindObjectsOfType<Lamp>();
+}
