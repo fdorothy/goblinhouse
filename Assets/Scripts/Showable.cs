fb4200250d09fb69d769f83c2af4191869267059
Fredric Dorothy <fredricdorothy@gmail.com>
2024-09-15 14:51:52 -0500

    added dead julia ghost scene

---
+++ Assets/Scripts/Showable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Hides the children of this gameobject if
+ * the content's variable is true.
+ */
+public class Showable : MonoBehaviour
+{
+    public string varName;
+    public bool invertLogic = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Show(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StateManager.singleton && StateManager.singleton.content)
+        {
+            bool state = (int)StateManager.singleton.content.story.variablesState[varName] != 0;
+            Show(state ^ invertLogic);
+        }
+    }
+
+    public void Show(bool visible)
+    {
+        for (int i=0; i<transform.childCount; i++)
+            transform.GetChild(i).gameObject.SetActive(visible);
+    }
+}
